cf0cb43b10189643f09b5e67e62ee5a433214e26
karanrumalwala <32751689+karanrumalwala@users.noreply.github.com>
2019-10-17 00:48:37 -0400

    Updates

---
+++ precache-manifest.720897044beffeb21c9b8de39d097ea5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48443623e40691190c0bcb85ba8b76ba",
+    "url": "/TodoList/index.html"
+  },
+  {
+    "revision": "70f5c48fca25371217c8",
+    "url": "/TodoList/static/css/main.7a64bec9.chunk.css"
+  },
+  {
+    "revision": "edd8489d4102ea6ad69e",
+    "url": "/TodoList/static/js/2.3b062e7c.chunk.js"
+  },
+  {
+    "revision": "70f5c48fca25371217c8",
+    "url": "/TodoList/static/js/main.b3cfb0ad.chunk.js"
+  },
+  {
+    "revision": "2cfbe7857b717cddbf6f",
+    "url": "/TodoList/static/js/runtime-main.482de22b.js"
+  }
+]);
